2a3c436db5742ad6d187e98ca153f779850c3c5f
steve-ongera <gadafisteve001@gmail.com>
2026-07-31 14:17:46 +0300

    fixed  a bug in leakage frontend page

--- frontend/src/pages/leakage/RevenueLeakageDashboard.jsx
+++ frontend/src/pages/leakage/RevenueLeakageDashboard.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { getLeakageDashboard, scanForLeaksNow } from "../../services/api";
 import { formatCurrency } from "../../utils/formatters";
 
